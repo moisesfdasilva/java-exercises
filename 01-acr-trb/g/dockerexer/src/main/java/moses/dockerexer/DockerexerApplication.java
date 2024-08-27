c17cf9db5cc6439de86379e1b29ee83af68b00ab
moisesfdasilva <msesfernandes@gmail.com>
2024-08-27 19:12:08 -0300

    feat: create a project "dockerexer" into 01-acr-trb/g.

---
+++ 01-acr-trb/g/dockerexer/src/main/java/moses/dockerexer/DockerexerApplication.java
@@ -0,0 +1,13 @@
+package moses.dockerexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerexerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerexerApplication.class, args);
+	}
+
+}
---
+++ 01-acr-trb/g/dockerexer/src/test/java/moses/dockerexer/DockerexerApplicationTests.java
@@ -0,0 +1,13 @@
+package moses.dockerexer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerexerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
